088d24e381296e0f146fff9da96e7fe4b9ca31ae
jpjoshuapeter <jpjoshuapeter@gmail.com>
2018-05-17 11:55:57 -0500

    change file type

---
+++ contact.php
@@ -0,0 +1,40 @@
+ <!DOCTYPE html>
+<html>
+    <head>
+        <Title>Submarine Sound</Title>
+        <Meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Kavivanar" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Oswald:400,500,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <Nav>
+            <div class="wrapper">
+                <ul>
+                    <li><a class="active" href="index.html">Submarine Sound</a></li>
+                    <li style="float:right"><a class="list" href="contact.html">Contact</a></li>
+                    <li style="float:right"><a class="list" href="about-us.html">About Us</a></li>
+                    <li style="float:right"><a class="list" href="products.html">Products</a></li>
+                </ul>
+            </Div>
+        </Nav>
+        <header class="masthead">
+            <form action="contactform.php" method="post">
+              <div class="email" >
+                  <h1>
+                      <p></p>
+                      <input type="email" name="EMAIL" placeholder="Email Address">
+                      <p></p>
+                      <input type="text" name="Name" placeholder="Name">
+                      <p></p>
+                      <input type="text" name="Subject" placeholder="Subject">
+                      <textarea class="message" name="Message" placeholder="Message"></textarea>
+                      <p></p>
+                      <input type="submit" name="submit" value="Send">
+                  </h1>
+              </div>
+            </form>
+        </header>
+    </body>
+</html>
